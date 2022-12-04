b7336303177d60064fa52bd8e9844beb749fa3c6
Vanshaj Poonia <Vanshajtheunique@gmail.com>
2022-12-04 08:28:41 +0530

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+
+import { makeStyles } from "@mui/styles";
+import { AppBar, Toolbar } from "@mui/material";
+
+const useStyles = makeStyles({
+    logo: {
+        width: 100,
+        padding: 5
+    },
+    navbar: {
+        background: ['#000', '!important'],
+        position: ['static', '!important'],
+        height: [50, '!important']
+    }
+})
+
+const Header = () => {
+    const classes = useStyles();
+    // const logo = 'https://miro.medium.com/max/802/1*dLWPk_rziSpWhPx1UWONbQ@2x.png';
+    const logo = 'https://test.hostman.com/img/social.png';
+
+    return (
+        <>
+            <img src={logo} alt="logo" className={classes.logo} />    
+            {/* <AppBar className={classes.navbar}>
+                <Toolbar>
+                </Toolbar>
+            </AppBar> */}
+        </>
+    )
+}
+
+export default Header;
